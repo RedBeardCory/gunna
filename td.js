6a2ceeea28a48fb5e105e2e922166fa3e5fb7245
RedBeardCory <corypatto@gmail.com>
2018-04-20 23:22:58 +1000

    added prompt for selection to complete a task

--- td.js
+++ td.js
@@ -36,22 +36,45 @@ if (args.length > 0) {
                 break;
             case '-d':
             case '--done':
+                getTasks()
+                .then(tasks => {
+                    prettyPrint(tasks);
+                    console.log(chalk.cyan('Please Enter the number of the completed task:'));
+                    process.stdin.on('data', buf => {
+                        text = buf.toString('ascii');
+                        // need to trim out the returns
+                        text = text.replace('\r', '');
+                        text = text.replace('\n', '');
+                        if (text === 'q' || text === 'quit') {
+                            process.stdin.pause();
+                        } else {
+                            let select = Number(text);
 
-                process.stdin.on('data', buf => {
-                    text = buf.toString('ascii');
-                    // need to trim out the returns
-                    if (text === 'q\r\n' || text === 'quit\r\n') {
-                        process.stdin.pause();
-                    }
-                    // task = ((i + 1) < args.length && args[i + 1].charAt(0) !== '-')? args[i + 1]: '';
-                    // i++;
-                    // while ((i + 1) < args.length && args[i + 1].charAt(0) !== '-') {
-                    //     // add to the main string
-                    //     task = task + ' ' + args[i + 1];
-                    //     // remove from the list of args
-                    //     i++;
-                    // }
-                    // console.log((!!task)? chalk.green.bold('Task Completed: ') + chalk.italic(task): chalk.yellow('ERROR:') + ' ' + chalk.cyan.underline('Must specify the name of the task!'));
+                            if (isNaN(select)) {
+                                console.log('That\'s not a number, this is a number: ');
+                                printAsciiNum(Math.floor(Math.random() * 9) + 1);
+                            } else if (select > tasks.list.length) {
+                                console.log('That number is too large');
+                            } else if (select < 1) {
+                                console.log('That number is too small');
+                            } else {
+                                // actually a valid selection
+                                completeTask(select - 1);
+                            }
+                        }
+                        // task = ((i + 1) < args.length && args[i + 1].charAt(0) !== '-')? args[i + 1]: '';
+                        // i++;
+                        // while ((i + 1) < args.length && args[i + 1].charAt(0) !== '-') {
+                        //     // add to the main string
+                        //     task = task + ' ' + args[i + 1];
+                        //     // remove from the list of args
+                        //     i++;
+                        // }
+                        // console.log((!!task)? chalk.green.bold('Task Completed: ') + chalk.italic(task): chalk.yellow('ERROR:') + ' ' + chalk.cyan.underline('Must specify the name of the task!'));
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
                 break;
             case '-l':
@@ -67,16 +90,11 @@ if (args.length > 0) {
                 // make a nice printout with ascii art and stuff
                 // loop over tasks and print
                 getTasks()
-                .then(json => {
-                    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
-                    for (var i = 0; i < json.list.length; i++) {
-                        process.stdout.write(chalk.green.bold('|') + chalk.bold(' [] ' + json.list[i].padEnd(45)) + chalk.green.bold('|\n'));
-                    }
-                    // console.log(chalk.green.bold('|                                                 |'))
-                    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
+                .then(tasks => {
+                    prettyPrint(tasks);
                 })
                 .catch(err => {
-                    console.log(chalk.red.bold('ERROR: ') + chalk.red.underline('Add a task first!'));
+                    console.log(err);
                 });
                 break;
             default:
@@ -132,9 +150,33 @@ function getTasks() {
 }
 
 function completeTask(int) {
+    fs.readFile(FilePath, 'utf8', (err, data) => {
+        let json = null;
+        // append the task
+        json = JSON.parse((data === null)? '{"tasks": {"list": [], "done": []}}': data);
+        json.tasks.done.push(int);
 
+        // write the json
+        fs.writeFile(FilePath, JSON.stringify(json, null, 4), err => {
+            if (err) throw err;
+            console.log(chalk.green.bold('Task Completed'));
+        })
+    });
 }
 
 function removeTask() {
 
+}
+
+function prettyPrint(tasks) {
+    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
+    for (var i = 0; i < tasks.list.length; i++) {
+        process.stdout.write(chalk.green.bold('|') + chalk.bold(' ' + (i + 1) + ' [] ' + tasks.list[i].padEnd(43)) + chalk.green.bold('|\n'));
+    }
+    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
+}
+
+// TODO: add ascii art here
+function printAsciiNum(num) {
+    console.log(num);
 }
