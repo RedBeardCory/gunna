5461da9ecdcbf205a607a721444bbdd56ddd8fbd
RedBeardCory <corypatto@gmail.com>
2018-04-23 23:03:43 +1000

    added readme. working done, remove commands

---
+++ readme.md
@@ -0,0 +1,50 @@
+# td
+
+#### By Cory Patterson
+
+## Why
+
+I wanted to expand my node cli skills and simultaneously wanted a nice looking todo list for work. So I created my own todo list and here it is. I also plan on taking this totally overboard so any weird and or wacky feature requests are welcome.
+
+## Install
+
+Clone the repo.
+Inside the repo do an `npm i`.
+Finally once that has installed then run `npm i -g`.
+Linux and Mac users may require sudo for the global install.
+
+Once installed with npm you can use the todo list anywhere within your terminal with the command `td`.
+
+## Usage
+
+There are a few simple flags for this program:
+
+`td -h || td --help`:
+
+Displays the list of command and what they do.
+
+`td -l || td --list`:
+
+Displays all the todo tasks.
+
+`td -a || td --add`:
+
+Add a task to the list.
+
+`td -r || td --remove`:
+
+Remove a task from the list.
+
+`td -d || td  --done`:
+
+Mark a task as complete.
+
+## Future Works
+
+Just some features that I thought could make this pretty cool:
+
+- [ ] Sync lists and settings across devices (via google drive or something)
+- [ ] Interactive selection instead of text prompts
+- [ ] Configuration options (colours and stuff)
+
+If you think there is an awesome feature just make an issue on this repo with the feature and explain what it does and I'll consider adding it to the program.
--- td.js
+++ td.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 const os = require('os');
 const util = require('util');
+const boxen = require('boxen');
 
 const FilePath = os.homedir() + '/todo.json';
 
@@ -18,7 +19,16 @@ if (args.length > 0) {
         switch (args[0]) {
             case '-h':
             case '--help':
-                console.log('Helpful hints');
+                console.log(chalk.bold.cyan(os.EOL + 'td -h td --help:') + os.EOL + os.EOL
+                + '    Displays the list of command and what they do.' + os.EOL + os.EOL
+                + chalk.bold.cyan('td -l || td --list:') + os.EOL + os.EOL
+                + '    Displays all the todo tasks.' + os.EOL + os.EOL
+                + chalk.bold.cyan('td -a || td --add:') + os.EOL + os.EOL
+                + '    Add a task to the list.' + os.EOL + os.EOL
+                + chalk.bold.cyan('td -r || td --remove:') + os.EOL + os.EOL
+                + '    Remove a task from the list.' + os.EOL + os.EOL
+                + chalk.bold.cyan('td -d || td  --done:') + os.EOL + os.EOL
+                + '    Mark a task as complete.' + os.EOL);
                 break;
             case '-a':
             case '--add':
@@ -43,8 +53,7 @@ if (args.length > 0) {
                     process.stdin.on('data', buf => {
                         text = buf.toString('ascii');
                         // need to trim out the returns
-                        text = text.replace('\r', '');
-                        text = text.replace('\n', '');
+                        text = text.replace(os.EOL, '');
                         if (text === 'q' || text === 'quit') {
                             process.stdin.pause();
                         } else {
@@ -60,17 +69,9 @@ if (args.length > 0) {
                             } else {
                                 // actually a valid selection
                                 completeTask(select - 1);
+                                // process.stdin.pause();
                             }
                         }
-                        // task = ((i + 1) < args.length && args[i + 1].charAt(0) !== '-')? args[i + 1]: '';
-                        // i++;
-                        // while ((i + 1) < args.length && args[i + 1].charAt(0) !== '-') {
-                        //     // add to the main string
-                        //     task = task + ' ' + args[i + 1];
-                        //     // remove from the list of args
-                        //     i++;
-                        // }
-                        // console.log((!!task)? chalk.green.bold('Task Completed: ') + chalk.italic(task): chalk.yellow('ERROR:') + ' ' + chalk.cyan.underline('Must specify the name of the task!'));
                     });
                 })
                 .catch(err => {
@@ -79,15 +80,6 @@ if (args.length > 0) {
                 break;
             case '-l':
             case '--list':
-                console.log(chalk.green.bold('LISTING'));
-                break;
-            case '-la':
-            case '--list-all':
-                console.log(chalk.green.bold('LISTING ALL, EVEN ') + chalk.bold.red('DELETED') + chalk.green.bold(' TASKS'));
-                break;
-            case '-p':
-            case '--pretty':
-                // make a nice printout with ascii art and stuff
                 // loop over tasks and print
                 getTasks()
                 .then(tasks => {
@@ -97,8 +89,43 @@ if (args.length > 0) {
                     console.log(err);
                 });
                 break;
+            case '-r':
+            case '--remove':
+                // remove a task
+                getTasks()
+                .then(tasks => {
+                    prettyPrint(tasks);
+                    console.log(chalk.cyan('Please Enter the number of the task to remove:'));
+                    process.stdin.on('data', buf => {
+                        text = buf.toString('ascii');
+                        // need to trim out the returns
+                        text = text.replace(os.EOL, '');
+                        if (text === 'q' || text === 'quit') {
+                            process.stdin.pause();
+                        } else {
+                            let select = Number(text);
+
+                            if (isNaN(select)) {
+                                console.log('That\'s not a number, this is a number: ');
+                                printAsciiNum(Math.floor(Math.random() * 9) + 1);
+                            } else if (select > tasks.list.length) {
+                                console.log('That number is too large');
+                            } else if (select < 1) {
+                                console.log('That number is too small');
+                            } else {
+                                // actually a valid selection
+                                removeTask(select - 1);
+                                // process.stdin.pause();
+                            }
+                        }
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+                break;
             default:
-                console.log('Argument not recognised: ' + args[i]);
+                console.log(chalk.red.bold('Argument not recognised: ') + args[0]);
         }
     // }
 }
@@ -146,7 +173,7 @@ function getTasks() {
             }
             resolve(json.tasks);
         });
-    })
+    });
 }
 
 function completeTask(int) {
@@ -160,20 +187,41 @@ function completeTask(int) {
         fs.writeFile(FilePath, JSON.stringify(json, null, 4), err => {
             if (err) throw err;
             console.log(chalk.green.bold('Task Completed'));
-        })
+        });
     });
 }
 
-function removeTask() {
+function removeTask(int) {
+    console.log(int);
+    fs.readFile(FilePath, 'utf8', (err, data) => {
+        let json = null;
 
+        json = JSON.parse((data === null)? '{"tasks": {"list": [], "done": []}}': data);
+
+        if (json.tasks.list[int]) {
+            json.tasks.list.splice(int, 1);
+            json.tasks.done.splice(json.tasks.done.indexOf(int), 1);
+            for (var i = 0; i < json.tasks.done.length; i++) {
+                if (json.tasks.done[i] > int) {
+                    json.tasks.done[i] = (json.tasks.done[i] - 1);
+                }
+            }
+            fs.writeFile(FilePath, JSON.stringify(json, null, 4), err => {
+                if (err) throw err;
+                console.log(chalk.green.bold('Task Removed'));
+            });
+        }
+    });
 }
 
 function prettyPrint(tasks) {
-    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
+    let string = '';
+
     for (var i = 0; i < tasks.list.length; i++) {
-        process.stdout.write(chalk.green.bold('|') + chalk.bold(' ' + (i + 1) + ' [] ' + tasks.list[i].padEnd(43)) + chalk.green.bold('|\n'));
+        string += (chalk.bold(' ' + (i + 1) + ' [' + (tasks.done.includes(i)? chalk.red('X'):' ') + '] ' + (tasks.done.includes(i)? chalk.dim(tasks.list[i]): tasks.list[i]) + os.EOL));
     }
-    console.log(chalk.green.bold('|>------>>>------>>> ToDo List <<<------<<<------<|'));
+
+    console.log(boxen(string, { padding: 1, borderColor: 'green', borderStyle: 'round', dimBorder: true }));
 }
 
 // TODO: add ascii art here
